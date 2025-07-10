d6f300464c94b16a1fbf688fcad2b2c0ccc2d8f4
Dondon Bautista <95321127+Yquannn@users.noreply.github.com>
2025-07-10 08:34:48 +0800

    Delete server directory

--- server/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# # path to the database file
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
